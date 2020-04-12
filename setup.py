e2936f1776f57044bc432af6531adaa35aac5290
Andreas Kvas <andreas@kvas.at>
2020-04-12 13:55:02 +0200

    added classifiers for PyPi

--- setup.py
+++ setup.py
@@ -6,11 +6,16 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='l3py',
-    version='0.1',
+    version='0.2',
     author='Andreas Kvas',
     description='A python package to convert potential coefficients to gridded mass anomalies',
     long_description=long_description,
     install_requires=['numpy', 'netcdf4', 'numpydoc'],
     packages=['l3py'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     package_data={'l3py': ['data/ddk_normals.npz', 'data/loadLoveNumbers_Gegout97.txt']}
 )
